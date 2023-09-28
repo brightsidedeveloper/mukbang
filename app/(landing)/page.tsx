887d9f1eba7bcaff9e890f272b8fd85eb70036ff
Tim <tim@brightsidedeveloper.com>
2023-09-27 21:25:09 -0500

    Save

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,9 +1,33 @@
 import { ThemeToggle } from '@/components/ThemeToggle'
+import Link from 'next/link'
 
 export default function page() {
   return (
     <div>
-      page <ThemeToggle />
+      <nav className='p-6 flex justify-between items-center bg-slate-200 dark:bg-slate-800'>
+        <h1 className='text-3xl'>Mukbang!</h1>
+        <ThemeToggle />
+      </nav>
+      <div className='h-full flex flex-col items-center gap-10 justify-center pt-10'>
+        <h1 className='text-3xl md:text-5xl  xl:text-7xl text-center'>
+          Welcome to Mukbang!
+        </h1>
+        {/* Sign in and sign up buttons */}
+        <div className='flex  gap-4'>
+          <Link
+            href='/sign-in'
+            className='bg-slate-200 dark:bg-slate-800 p-4 rounded-lg text-center'
+          >
+            Sign in
+          </Link>
+          <Link
+            href='/sign-up'
+            className='bg-slate-200 dark:bg-slate-800 p-4 rounded-lg text-center'
+          >
+            Sign up
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
--- app/app/(home)/page.tsx
+++ app/app/(home)/page.tsx
@@ -1,3 +1,13 @@
+import { SignOutButton } from '@clerk/nextjs'
+import { LogOutIcon } from 'lucide-react'
+
 export default function page() {
-  return <div>Home</div>
+  return (
+    <div>
+      Home
+      <SignOutButton>
+        <LogOutIcon className='cursor-pointer' />
+      </SignOutButton>
+    </div>
+  )
 }
