5138a75a3273a0cf23858ae92e3a0ce49e9e94b9
Tim <tim@brightsidedeveloper.com>
2023-09-29 00:40:05 -0500

    Try me

--- app/app/(home)/page.tsx
+++ app/app/(home)/page.tsx
@@ -1,8 +1,21 @@
 import { Button } from '@/components/ui/button'
-import { Switch } from '@/components/ui/switch'
 import Link from 'next/link'
 import Themer from './Themer'
 
+export async function generateMetaData({
+  searchParams: { theme },
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) {
+  return {
+    themeColor:
+      theme === 'dark-blue'
+        ? '#020817'
+        : theme === 'dark-blue'
+        ? '#0c0a09'
+        : '#FFFFFF',
+  }
+}
 export default async function page() {
   return (
     <div className='flex flex-col gap-10'>
--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -4,29 +4,7 @@ import { LogOutIcon } from 'lucide-react'
 import DyanmicHomeLink from './DynamicHomeLink'
 import CommandMenu from '@/components/CommandMenu'
 
-export async function generateMetaData({
-  searchParams: { theme },
-}: {
-  searchParams: { [key: string]: string | string[] | undefined }
-}) {
-  return {
-    themeColor:
-      theme === 'dark-blue'
-        ? '#020817'
-        : theme === 'dark-blue'
-        ? '#0c0a09'
-        : '#FFFFFF',
-  }
-}
-
-export default function layout({
-  children,
-  searchParams,
-}: {
-  children: React.ReactNode
-  searchParams: { [key: string]: string | string[] | undefined }
-}) {
-  console.log('~~', searchParams)
+export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='h-full'>
       <header className='sticky z-10 backdrop-blur top-0 px-6 h-16 flex justify-between items-center border-b-[1px] shadow-md border-muted-background/30 bg-background/90 '>
