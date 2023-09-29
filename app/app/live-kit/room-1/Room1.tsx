5fbc0ed44e1818b1b098429b3b2c494599617c15
Tim <tim@brightsidedeveloper.com>
2023-09-28 23:15:37 -0500

    Co-authored-by: Wes Nelson <WesNelson10@users.noreply.github.com>

--- app/app/live-kit/room-1/Room1.tsx
+++ app/app/live-kit/room-1/Room1.tsx
@@ -1,8 +1,9 @@
 'use client'
 
 import '@livekit/components-styles'
-import { LiveKitRoom, VideoConference, Chat } from '@livekit/components-react'
+import { LiveKitRoom, useChat, Chat } from '@livekit/components-react'
 import { useEffect, useState } from 'react'
+import { Switch } from '@/components/ui/switch'
 
 export default function Room1({ uid }: { uid: string }) {
   const room = 'room-1'
@@ -20,18 +21,31 @@ export default function Room1({ uid }: { uid: string }) {
       }
     })()
   }, [name])
-
   if (token === '') {
     return <div>Getting token...</div>
   }
 
   return (
     <LiveKitRoom token={token} serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_URL}>
-      <Chat />
+      <ChatRoom />
     </LiveKitRoom>
   )
 }
 
-// function ChatRoom() {
-//   const
-// }
+function ChatRoom() {
+  const [checked, setChecked] = useState(false)
+  const { send, chatMessages } = useChat()
+
+  useEffect(() => {
+    if (chatMessages.length === 0) return
+    setChecked(chatMessages[chatMessages.length - 1].message === 'on')
+  }, [chatMessages])
+
+  if (!send) return null
+  const toggle = (state: boolean) => {
+    setChecked(state)
+    send(state ? 'off' : 'on')
+  }
+
+  return <Switch checked={checked} onCheckedChange={toggle} />
+}
