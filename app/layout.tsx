ba9c4ad8763d7ce0a3e3b72d9ea3da184b1b666e
Tim <tim@brightsidedeveloper.com>
2023-09-28 23:22:38 -0500

    Solved Hydration Error probably

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang='en'>
+      <html lang='en' suppressHydrationWarning>
         <body className={inter.className}>
           <ThemeProvider
             attribute='class'
