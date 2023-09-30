aef3b4d813b70604f07b6201ff13c999223accb5
Tim <tim@brightsidedeveloper.com>
2023-09-29 21:41:51 -0500

    Better now

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div className='h-full flex flex-col lg:flex-row items-center gap-10 justify-center'>
-      <h1 className='text-3xl md:text-5xl  xl:text-7xl text-center'>
+      <h1 className='text-3xl md:text-5xl xl:text-7xl text-center'>
         Welcome to Mukbang!
       </h1>
       {children}
