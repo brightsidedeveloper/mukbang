7a0d671e394ee1a618d7681e64bb743988d6d7a1
Tim <timvan0118@gmail.com>
2023-11-14 11:25:12 +0400

    Weird

--- app/app/live-kit/loading.tsx
+++ app/app/live-kit/loading.tsx
@@ -1,11 +1,12 @@
-import { Skeleton } from '@/components/ui/skeleton'
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function loading() {
   return (
     <div>
-      <div className='flex gap-4 flex-wrap'>
-        <Skeleton className='w-32 h-10' />
-        <Skeleton className='w-28 h-10' />
+      <div className="flex gap-4 flex-wrap">
+        <Skeleton className="w-32 h-10" />
+        <Skeleton className="w-28 h-10" />
+        <Skeleton className="w-28 h-10" />
       </div>
     </div>
   )
